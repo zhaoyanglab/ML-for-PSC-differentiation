c44ff006850e54f6f60e50807cbc0f6ad8ae335b
chendaichao <67040079+chendaichao@users.noreply.github.com>
2024-10-08 12:03:37 +0800

    Update code for better clarity.

--- stage_II/Evaluation/Pearson_correlation.m
+++ stage_II/Evaluation/Pearson_correlation.m
@@ -1,46 +1,63 @@
-mainpath_ctnt = 'E:\MyData\PHD\WeaklySupervisedLearning\新数据\aug\aug\CD13';        % Path of the cTNT fluorescent image
-imgpath_ctnt = dir(mainpath_ctnt);
-for i = 1:length(imgpath_ctnt) - 2
-    Imgpath_ctnt = fullfile(imgpath_ctnt(i+2).folder,imgpath_ctnt(i+2).name);
-    differentiation_rate(i) = differentiation(Imgpath_ctnt);
+result = {'Batch_ID' 'S_id' 'predicted proportion of CM-committed CPCs' 'true differentiation efficiency'};
+batch_paths = dir('../img_result_wholewell/binary/CD*');
+
+fluorescence_thres = 0.5;
+n = 0;
+for batch_id = 1:numel(batch_paths)
+    batch_name = batch_paths(batch_id).name;
+
+    mask_predicted_paths = dir(fullfile('../img_result_wholewell/binary', batch_name, '*.png'));
+    mask_predicted_paths = sortfiles(mask_predicted_paths);
+
+    ctnt_paths = dir(fullfile('../data/Test', batch_name, 'cTnT/*.png'));
+    ctnt_paths = sortfiles(ctnt_paths);
+
+    for i = 1:numel(mask_predicted_paths)
+
+        n = n + 1;
+
+        img_name = mask_predicted_paths(i).name;
+        % Path of the predicted binarized segmentation mask of CPC
+        mask_pred_path = fullfile(mask_predicted_paths(i).folder, img_name);
+        % Path of the manually labeled mask
+        ctnt_path = fullfile(ctnt_paths(i).folder,ctnt_paths(i).name);
+
+        % The predicted mask
+        mask_pred = imread(mask_pred_path);
+        % Compute the proportion of CM-committed CPCs
+        proportion_pred(n) = mean(mean(mask_pred == 255));
+
+        % The cTnT fluorescence label
+        ctnt = double(imread(ctnt_path)) / 255;
+        ctnt = imresize(ctnt, [2816, 2816]);
+        differentiation_efficiency(n) = sum(ctnt(ctnt > fluorescence_thres))/numel(ctnt);
+    
+        result = [result; { ...
+                batch_name img_name proportion_pred(n) differentiation_efficiency(n) ...
+        }];
+    end
+
 end
 
-mainpath_predict = 'E:\MyData\PHD\WeaklySupervisedLearning\新数据\CD13\img_BW_splice\NotBad';    % Path of the predicted binary image
-imgpath_predict = dir(mainpath_predict);
-for i = 1:length(imgpath_predict) - 2
-    Imgpath_predict = fullfile(imgpath_predict(i+2).folder,imgpath_predict(i+2).name);
-    img_predict = double(imread(Imgpath_predict));
-    differentiation_rate_pre(i) = length(find(img_predict == 255))/numel(img_predict);
-%     differentiation_rate_pre(i) = differentiation(Imgpath_predict);
-end
+writecell(result, 'whole_well_comparion.csv', 'Delimiter', ',');
+
 % Pearson correlation
-[R,P] = corrcoef(differentiation_rate',differentiation_rate_pre');
+[R,P] = corrcoef(differentiation_efficiency', proportion_pred');
+fprintf('r = %.2f, P = %.6f\n', R(1, 2), P(1, 2));
+
 % figure
-p = polyfit(differentiation_rate', differentiation_rate_pre', 1); % fitted coeffcient p
-yFit = polyval(p, differentiation_rate'); % estimate fitted Y with X and p.
-figure(9),
+p = polyfit(differentiation_efficiency', proportion_pred', 1); % fitted coeffcient p
+yFit = polyval(p, differentiation_efficiency'); % estimate fitted Y with X and p.
+figure;
 set(gcf,'InvertHardCopy','off','color','white');
-hold all,
-scatter(differentiation_rate', differentiation_rate_pre');
-plot(differentiation_rate',yFit,'-');
-xlabel('differentiation rate');
-ylabel('differentiation rate pre');
-% title(sprintf('p = %g', pval));
+scatter(differentiation_efficiency', proportion_pred' * 100);
+hold on;
+plot(differentiation_efficiency',yFit * 100,'-');
+xlabel('Differentiation efficiency index');
+ylabel('Predicted % of CPC regions');
+ylim([0 100]);
 
-function differentiation_rate = differentiation(img_dir)
-img = im2double(imread(img_dir));
-% img = img(1:5632,1:5632);
-img = imresize(img,[2816,2816]);
-% img_norm = mat2gray(img);
-img(img > 0.6) = 0;
-level = 0.15;
-differentiation_rate = sum(img(img>level))/numel(img);
 
-% img_bw = im2bw(img_norm,level);
-% img_bw = img_bw(1:5632,1:5632);
-% figure(1);
-% imshow(img_bw);
-% figure(2);
-% imshow(img_norm);
-% differentiation_rate = length(find(img_bw == 1))/numel(img_bw);
+function ret = sortfiles(files)
+    ret = table2struct(sortrows(struct2table(files), 'name'));
 end
--- stage_II/Evaluation/index_calculate.m
+++ stage_II/Evaluation/index_calculate.m
@@ -1,26 +1,70 @@
-img_pre_path = dir('E:\MyData\PHD\WeaklySupervisedLearning\dd\result\pre_1');       % Path of the predicted binarized image
-img_path = dir('E:\MyData\PHD\WeaklySupervisedLearning\dd\result\gt_1');            % Path of the manually labeling mask
-img_num = length(img_path) - 2;
-for i = 1:img_num
-    img_pre = fullfile(img_pre_path(i+2).folder,img_pre_path(i+2).name);
-    img = fullfile(img_path(i+2).folder,img_path(i+2).name);
-    D5_mask = im2double(imread(img));
-    D5_mask = imresize(D5_mask,[2816,2816]);
-    D5_mask_pre = im2double(imread(img_pre));
-    D5_mask_pre = imresize(D5_mask_pre,[2816,2816]);  
-    D5 = D5_mask + D5_mask_pre;
-    Positive = length(find(D5_mask == 1));
-    Negative = length(find(D5_mask == 0));
-    Pre_Positive = length(find(D5_mask_pre == 1));
-    Pre_Negative = length(find(D5_mask_pre == 0));
-    Intersection = length(find(D5 == 2));
-    Intersection_N = length(find(D5 == 0));
-    Union = Intersection +  length(find(D5 == 1));
-    IOU(i) = Intersection/Union;
-    Ture_Positive(i) = Intersection/Positive;
-    False_Positive(i) = (Pre_Positive - Intersection)/Negative;
-    ACC(i) = (Intersection + Intersection_N)/(Positive + Negative);
-    PPV(i) = Intersection/Pre_Positive;
-    TNR(i) = Intersection_N/Negative;
-    F1(i) = 2/(1/Ture_Positive(i) + 1/PPV(i));
+result = {'Batch_ID' 'S_id' 'accuracy' 'F1 score' 'precision' 'recall' 'specificity' 'IoU'};
+
+n = 0;
+
+eps = 0.01;
+batch_paths = dir('../img_result_wholewell/binary/CD*');
+for batch_id = 1:numel(batch_paths)
+    batch_name = batch_paths(batch_id).name;
+    
+    mask_predicted_paths = dir(fullfile('../img_result_wholewell/binary', batch_name, '*.png'));
+    mask_predicted_paths = sortfiles(mask_predicted_paths);
+    
+    mask_gt_paths = dir(fullfile('../data/Test', batch_name, 'Mask/*.png'));
+    mask_gt_paths = sortfiles(mask_gt_paths);
+
+    mkdir(fullfile('../img_result_wholewell/binary_gt', batch_name));
+    
+    for i = 1:numel(mask_predicted_paths)
+        img_name = mask_predicted_paths(i).name;
+
+        % Path of the predicted binarized segmentation mask of CPC
+        mask_pred_path = fullfile(mask_predicted_paths(i).folder, img_name);
+        % Path of the manually labeled mask
+        mask_gt_path = fullfile(mask_gt_paths(i).folder,mask_gt_paths(i).name);
+
+        % the manually labeled mask
+        mask_gt = imread(mask_gt_path);
+        mask_gt = imresize(mask_gt, [2816,2816]);
+        mask_gt = double(mask_gt > 0); 
+        imwrite(cast(mask_gt * 255, 'uint8'), fullfile('../img_result_wholewell/binary_gt', batch_name, mask_gt_paths(i).name));
+        
+        % the predicted mask
+        mask_pred = double(imread(mask_pred_path) > 0);
+
+        Positive_GT = sum(sum(mask_gt == 1));
+        Negative_GT = sum(sum(mask_gt == 0));
+        Positive_Pred = sum(sum(mask_pred == 1));
+        Negative_Pred = sum(sum(mask_pred == 0));
+        Positive_Intersection = sum(sum(mask_pred + mask_gt == 2));
+        Negative_Intersection = sum(sum(mask_pred + mask_gt == 0));
+        Union = Positive_Intersection + sum(sum(mask_pred + mask_gt == 1));
+
+        % for pixel-wise measurements, a well in taken into account if it contains target cells
+        if Positive_GT / numel(mask_gt) >= eps
+            n = n + 1;
+            IOU(n) = Positive_Intersection / Union; % intersection-over-union
+            True_Positive(n) = Positive_Intersection/Positive_GT; % true positive rate (i.e., recall)
+            ACC(n) = (Positive_Intersection + Negative_Intersection)/(Positive_GT + Negative_GT); % accuracy
+            PPV(n) = Positive_Intersection/Positive_Pred; % positive predictive value (i.e., precision)
+            TNR(n) = Negative_Intersection / Negative_GT; % true negative rate (i.e., specificity) 
+            F1(n) = 2/(1/True_Positive(n) + 1/PPV(n)); % F1 score
+
+            result = [result; { ...
+                batch_name img_name ACC(n) F1(n) PPV(n) True_Positive(n) TNR(n) IOU(n) ...
+                }];
+        end
+    end
+end
+
+writecell(result, 'pixel_comparison.csv', 'Delimiter', ',');
+fprintf("Accuracy (%%) = %.1f ± %.1f\n", mean(ACC, 'omitNaN') * 100, std(ACC, 'omitNaN') * 100);
+fprintf("F1 (%%) = %.1f ± %.1f\n", mean(F1, 'omitNaN') * 100, std(F1, 'omitNaN') * 100);
+fprintf("Precision (%%) = %.1f ± %.1f\n", mean(PPV, 'omitNaN') * 100, std(PPV, 'omitNaN') * 100);
+fprintf("Recall (%%) = %.1f ± %.1f\n", mean(True_Positive, 'omitNaN') * 100, std(True_Positive, 'omitNaN') * 100);
+fprintf("Specificity (%%) = %.1f ± %.1f\n", mean(TNR, 'omitNaN') * 100, std(TNR, 'omitNaN') * 100);
+fprintf("IoU (%%) = %.1f ± %.1f\n", mean(IOU, 'omitNaN') * 100, std(IOU, 'omitNaN') * 100);
+
+function ret = sortfiles(files)
+    ret = table2struct(sortrows(struct2table(files), 'name'));
 end
